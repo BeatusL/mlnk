0bb1c4e78a9ef9eaf41c4422fe7f3bdf5298af54
BeatusL <vagner.aa@edu.spbstu.ru>
2023-05-12 18:06:09 +0300

    mlnk

--- lwjgl3/src/main/java/com/BeatusL/Malinka/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/java/com/BeatusL/Malinka/lwjgl3/Lwjgl3Launcher.kt
@@ -1,30 +1,41 @@
-package com.BeatusL.Malinka.lwjgl3;
+package com.BeatusL.Malinka.lwjgl3
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.BeatusL.Malinka.Malinka;
+import com.BeatusL.Malinka.Malinka
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 
-/** Launches the desktop (LWJGL3) application. */
-public class Lwjgl3Launcher {
-    public static void main(String[] args) {
-        createApplication();
+/** Launches the desktop (LWJGL3) application.  */
+object Lwjgl3Launcher {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        createApplication()
     }
 
-    private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Malinka(), getDefaultConfiguration());
+    private fun createApplication(): Lwjgl3Application {
+        return Lwjgl3Application(Malinka(), defaultConfiguration)
     }
 
-    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
-        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle("Malinka");
-        configuration.useVsync(true);
-        //// Limits FPS to the refresh rate of the currently active monitor.
-        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
-        //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
-        //// useful for testing performance, but can also be very stressful to some hardware.
-        //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-        configuration.setWindowedMode(640, 480);
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
-        return configuration;
-    }
+    //// Limits FPS to the refresh rate of the currently active monitor.
+    //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
+    //// useful for testing performance, but can also be very stressful to some hardware.
+    //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
+    private val defaultConfiguration: Lwjgl3ApplicationConfiguration
+        get() {
+            val configuration = Lwjgl3ApplicationConfiguration()
+            configuration.setTitle("Malinka")
+            configuration.useVsync(true)
+            //// Limits FPS to the refresh rate of the currently active monitor.
+            configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate)
+            //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
+            //// useful for testing performance, but can also be very stressful to some hardware.
+            //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
+            configuration.setWindowedMode(640, 480)
+            configuration.setWindowIcon(
+                "libgdx128.png",
+                "libgdx64.png",
+                "libgdx32.png",
+                "libgdx16.png"
+            )
+            return configuration
+        }
 }
